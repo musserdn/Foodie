eba247b2acda8a031e4d722642009b78ed347e67
ngin2894 <nataliegindraux@gmail.com>
2024-11-22 17:11:46 -0600

    made changes to css, index.html, and javascript file.  Added flexbox properties to the logo & title for resizing, changed color scheme, added a navbar, created 3 new html pages & added redirect logic in the javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ document.getElementById('CF1').addEventListener('click', function() {
 document.getElementById('CF2').addEventListener('click', function() {
     $('.ui.modal').modal('show');
 });
+
+const aboutButton = document.getElementById('aboutbtn');
+const recipeButton = document.getElementById('recipebtn');
+const listButton = document.getElementById('listbtn');
+
+aboutButton.addEventListener('click', () => {
+    window.location.href = 'about.html';
+});
+
+recipeButton.addEventListener('click', () => {
+    window.location.href = 'recipe.html';
+});
+
+listButton.addEventListener('click', () => {
+    window.location.href = 'shopping.html';
+});
