24b35b13d7d083bed9e3b7d63f65a70c813625bb
musserdn <musserdn@gmail.com>
2024-11-24 16:21:50 -0600

    committing the modal changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
-document.getElementById('CF1').addEventListener('click', function() {
-    $('.ui.modal').modal('show');
-});
-document.getElementById('CF2').addEventListener('click', function() {
-    $('.ui.modal').modal('show');
+// document.getElementById('CF1').addEventListener('click', function() {
+    // $('#CFM1').modal('show');
+// });
+// document.getElementById('CF2').addEventListener('click', function() {
+    // $('#CFM2').modal('show');
+// });
+// Add click event listener to all elements starting with 'CF'
+document.querySelectorAll('[id^="CF"]').forEach(element => {
+    element.addEventListener('click', function() {
+        const modalNumber = this.id.replace('CF', '');
+        $(`#CFM${modalNumber}`).modal('show');
+    });
 });
 
 // menu button functionality to navigate to other pages
