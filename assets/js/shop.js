2443ebab08fffb96043fcc2f90d1e9320908f320
ngin2894 <nataliegindraux@gmail.com>
2024-11-24 16:32:35 -0600

    added links for javascript

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,11 +1,3 @@
-document.getElementById('CF1').addEventListener('click', function() {
-    $('.ui.modal').modal('show');
-});
-document.getElementById('CF2').addEventListener('click', function() {
-    $('.ui.modal').modal('show');
-});
-
-// menu button functionality to navigate to other pages
 const aboutButton = document.getElementById('aboutbtn');
 const recipeButton = document.getElementById('recipebtn');
 const listButton = document.getElementById('listbtn');
